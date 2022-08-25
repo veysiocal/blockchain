ca948e6d6e50aa35e63811568b41d713ec777b2e
Veysi Öcal <veysiocal@hotmail.com>
2022-08-25 18:27:06 +0300

    adding getParents

--- contracts/InfinityContract.sol
+++ contracts/InfinityContract.sol
@@ -7,6 +7,8 @@ contract InfinityContract {
 
     constructor() {
         owner = msg.sender;
+        Admin storage adminOwner = admins[owner];
+        adminOwner.walletaddress = owner;
     }
 
     struct Child {
@@ -27,120 +29,194 @@ contract InfinityContract {
         address[] childs;
     }
 
-    mapping(address => Parent)  public parents;
-    mapping(address => Child)  public childs; 
+    struct Admin {
+        address walletaddress;
+        uint parentIndex;
+        address[] parents;
+    }
+
+    mapping(address => Parent) public parents;
+    mapping(address => Child) public childs;
+    mapping(address => Admin) public admins;
 
     event TransferToContract(address from, address to, uint amount);
 
-    function sendMoney() public onlyParent payable{
+    function sendMoney() public payable onlyParent {
         parents[msg.sender].currentBalance += msg.value;
         emit TransferToContract(msg.sender, address(this), msg.value);
     }
 
-    function sendMoneyToChild(address _childAddress, uint _amount) public onlyParent{
-        require(parents[msg.sender].currentBalance >= _amount, "You have not that much ether");
+    function sendMoneyToChild(address _childAddress, uint _amount)
+        public
+        onlyParent
+    {
+        require(
+            parents[msg.sender].currentBalance >= _amount,
+            "You have not that much ether"
+        );
 
         parents[msg.sender].currentBalance -= _amount;
         childs[_childAddress].amountOfMoney += _amount;
     }
 
-    function showMyBalance() public onlyParent view returns(uint) {
+    function showMyBalance() public view onlyParent returns (uint) {
         return parents[msg.sender].currentBalance;
     }
 
-    function showBalanceOfContract() public onlyAdmin view returns(uint){
+    function showBalanceOfContract() public view onlyAdmin returns (uint) {
         return address(this).balance;
     }
 
-    function addParent(string memory _name, string memory _surname) public{
+    function addParent(string memory _name, string memory _surname) public {
         address payable sender = payable(msg.sender);
 
         Parent storage parent = parents[sender];
 
-        require(parent.walletaddress != sender, "This parent is already added!") ;
+        require(
+            parent.walletaddress != sender,
+            "This parent is already added!"
+        );
 
         parent.name = _name;
         parent.surname = _surname;
         parent.walletaddress = sender;
+
+        admins[owner].parents.push(parent.walletaddress);
+        admins[owner].parentIndex++;      
     }
 
-    function addChild(string memory _childname, uint _releaseTime, address payable _childAddress) public onlyParent{
+    function addChild(
+        string memory _childname,
+        uint _releaseTime,
+        address payable _childAddress
+    ) public onlyParent {
         Child storage child = childs[_childAddress];
 
-        require(child.walletaddress != _childAddress, "This child is already added!");
-        Child memory childNew = Child(_childAddress, msg.sender, _childname, _releaseTime, 0, 0);
+        require(
+            child.walletaddress != _childAddress,
+            "This child is already added!"
+        );
+        Child memory childNew = Child(
+            _childAddress,
+            msg.sender,
+            _childname,
+            _releaseTime,
+            0,
+            0
+        );
 
         parents[msg.sender].childs.push(childNew.walletaddress);
         parents[msg.sender].childIndex++;
 
-        childs[_childAddress] = childNew; 
-
+        childs[_childAddress] = childNew;
     }
 
-    function getChilds() public view onlyParent returns(Child[] memory) {
-        Child[] memory childsOfParent = new Child[](parents[msg.sender].childIndex);
+    function getChilds() public view onlyParent returns (Child[] memory) {
+        Child[] memory childsOfParent = new Child[](
+            parents[msg.sender].childIndex
+        );
 
-        for(uint i = 0; i < parents[msg.sender].childIndex; i++) {
+        for (uint i = 0; i < parents[msg.sender].childIndex; i++) {
             childsOfParent[i] = (childs[parents[msg.sender].childs[i]]);
         }
         return childsOfParent;
     }
 
-    function changeReleaseTime(address _childAddress, uint _newReleaseTime) public onlyParent{
-        require(childs[_childAddress].parentAddress == msg.sender, "This child is not your child!");
+    function getParents() public view onlyParent returns (Parent[] memory) {
+        Parent[] memory adminParents = new Parent[](admins[owner].parentIndex);
+
+        for (uint i = 0; i < admins[owner].parentIndex; i++) {
+            adminParents[i] = (parents[admins[owner].parents[i]]);
+        }
+        return adminParents;
+    }
+
+    function changeReleaseTime(address _childAddress, uint _newReleaseTime)
+        public
+        onlyParent
+    {
+        require(
+            childs[_childAddress].parentAddress == msg.sender,
+            "This child is not your child!"
+        );
         childs[_childAddress].releaseTime = _newReleaseTime;
     }
 
-    function withdrawMoneyByParent(uint _amount) public onlyParent{
-        require(parents[msg.sender].currentBalance >= _amount, "You are not that much ether");
+    function withdrawMoneyByParent(uint _amount) public onlyParent {
+        require(
+            parents[msg.sender].currentBalance >= _amount,
+            "You are not that much ether"
+        );
 
         parents[msg.sender].currentBalance -= _amount;
 
         payable(msg.sender).transfer(_amount);
-
     }
 
-    function withdrawAllMoneyByParent() public onlyParent{
-        require(parents[msg.sender].currentBalance != 0, "You are not have any ehter");
+    function withdrawAllMoneyByParent() public onlyParent {
+        require(
+            parents[msg.sender].currentBalance != 0,
+            "You are not have any ehter"
+        );
 
         uint amountOfAccessableMoney = parents[msg.sender].currentBalance;
 
         parents[msg.sender].currentBalance = 0;
 
         payable(msg.sender).transfer(amountOfAccessableMoney);
-
     }
 
-    function withdrawMoneyByParentFromChild(address _childAddress, uint _amount) public onlyParent{
+    function withdrawMoneyByParentFromChild(address _childAddress, uint _amount)
+        public
+        onlyParent
+    {
+        require(
+            childs[_childAddress].releaseTime > block.timestamp,
+            "You are not allowed withdraw this money. Money is allowed to usage of child now."
+        );
 
-        require(childs[_childAddress].releaseTime > block.timestamp, "You are not allowed withdraw this money. Money is allowed to usage of child now.");
-
-        require(childs[_childAddress].amountOfMoney >= _amount, "This child have not that much ether");
+        require(
+            childs[_childAddress].amountOfMoney >= _amount,
+            "This child have not that much ether"
+        );
 
         parents[msg.sender].currentBalance += _amount;
 
         childs[_childAddress].amountOfMoney -= _amount;
     }
 
-    function withdrawAllMoneyByParentFromChild(address _childAddress) public onlyParent{
+    function withdrawAllMoneyByParentFromChild(address _childAddress)
+        public
+        onlyParent
+    {
+        require(
+            childs[_childAddress].releaseTime > block.timestamp,
+            "You are not allowed withdraw this money. Money is allowed to usage of child now. ?? fwfwfwfwfwfw"
+        );
 
-        require(childs[_childAddress].releaseTime > block.timestamp, "You are not allowed withdraw this money. Money is allowed to usage of child now.");
-
-        require(childs[_childAddress].amountOfMoney != 0, "This child have not any ether");
+        require(
+            childs[_childAddress].amountOfMoney != 0,
+            "This child have not any ether"
+        );
 
         uint amountOfMoney = childs[_childAddress].amountOfMoney;
 
         childs[_childAddress].amountOfMoney = 0;
 
         parents[msg.sender].currentBalance += amountOfMoney;
-
     }
 
-    function withdrawMoneyByChild(uint _amount) public onlyChild{
+    function withdrawMoneyByChild(uint _amount) public onlyChild {
         address walletaddress = msg.sender;
 
-        require(childs[walletaddress].releaseTime <= block.timestamp, "You are not allowed withdraw money yet");
-        require(childs[walletaddress].amountOfMoney >= _amount, "You are not have that much ether");
+        require(
+            childs[walletaddress].releaseTime <= block.timestamp,
+            "You are not allowed withdraw money yet"
+        );
+        require(
+            childs[walletaddress].amountOfMoney >= _amount,
+            "You are not have that much ether"
+        );
 
         childs[walletaddress].amountOfMoney -= _amount;
 
@@ -149,12 +225,15 @@ contract InfinityContract {
         payable(msg.sender).transfer(_amount);
     }
 
-    function withdrawAllMoneyByChild() public onlyChild{
+    function withdrawAllMoneyByChild() public onlyChild {
         address walletaddress = msg.sender;
 
         uint amountOfMoneyChildHave = childs[walletaddress].amountOfMoney;
 
-        require(childs[walletaddress].releaseTime <= block.timestamp, "You are not allowed withdraw money yet");
+        require(
+            childs[walletaddress].releaseTime <= block.timestamp,
+            "You are not allowed withdraw money yet"
+        );
         require(amountOfMoneyChildHave != 0, "You are not have any ether");
 
         childs[walletaddress].amountOfMoney = 0;
@@ -162,23 +241,21 @@ contract InfinityContract {
         payable(msg.sender).transfer(amountOfMoneyChildHave);
     }
 
-    function getRole() public view returns(string memory) {
+    function getRole() public view returns (string memory) {
         address sender = msg.sender;
 
         Parent storage parent = parents[sender];
         Child storage child = childs[sender];
 
-        if(owner == sender) {
+        if (owner == sender) {
             return "admin";
-        }
-        else if(parent.walletaddress == sender) {
+        } else if (parent.walletaddress == sender) {
             return "parent";
-        } else if(child.walletaddress == sender) {
+        } else if (child.walletaddress == sender) {
             return "child";
         } else {
             return "none";
         }
-
     }
 
     modifier onlyAdmin() {
